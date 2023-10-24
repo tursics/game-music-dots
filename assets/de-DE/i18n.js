ea105e30c62058fd5c0766768ef2b4fe56b3d76b
Thomas Tursics <thomas@tursics.de>
2023-10-24 23:02:51 +0200

    initial commit

---
+++ assets/de-DE/i18n.js
@@ -0,0 +1,16 @@
+﻿if( typeof i18n === "undefined") i18n = new Array();
+
+i18n['de'] = {
+	'appLang': 'de-DE',
+	'appTitle': 'Musik-Punkte',
+
+	'load_loading': 'Wird geladen ...',
+
+	'welcome_play': 'Spielen!',
+
+	'levels_select': 'Level auswählen',
+
+	'board_score': 'Punkte:',
+	'board_moves': 'Züge:',
+	'board_goal': 'Ziel:',
+};
---
+++ assets/en-US/i18n.js
@@ -0,0 +1,16 @@
+﻿if( typeof i18n === "undefined") i18n = new Array();
+
+i18n['en'] = {
+	'appLang': 'en-US',
+	'appTitle': 'Music Dots',
+
+	'load_loading': 'Loading ...',
+
+	'welcome_play': 'Play!',
+
+	'levels_select': 'Level Select',
+
+	'board_score': 'Score:',
+	'board_moves': 'Moves:',
+	'board_goal': 'Goal:',
+};
