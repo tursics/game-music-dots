1f379e44bed86cbe4cb64c45de3b5214dd076888
Thomas Tursics <thomas@tursics.de>
2023-10-27 20:34:59 +0200

    refactoring: move code to new module.sound.js

--- assets/index.js
+++ assets/index.js
@@ -1,75 +1,17 @@
 var divGame = null;
-var guitarNeck = [
-	// https://de.wikipedia.org/wiki/Frequenzen_der_gleichstufigen_Stimmung
-	{color:'gray',   title:'c1', frequency: '261.626'},
-	{color:'black',  title:'cis1', frequency: '277.183'},
-	{color:'red',    title:'d1', frequency: '293.665'},
-	{color:'yellow', title:'dis1', frequency: '311.127'},
-	{color:'gray',   title:'e1', frequency: '329.628'},
-	{color:'gray',   title:'f1', frequency: '349.228'},
-	{color:'gray',   title:'fis1', frequency: '369.994'},
-	{color:'gray',   title:'g1', frequency: '391.995'},
-	{color:'gray',   title:'gis1', frequency: '415.305'},
-	{color:'gray',   title:'a1', frequency: '440'},
-	{color:'gray',   title:'b', frequency: '466.164'},
-	{color:'gray',   title:'h1', frequency: '493.883'},
-	{color:'gray',   title:'c2', frequency: '523.251'},
-	{color:'gray',   title:'cis2', frequency: '554.365'},
-	{color:'gray',   title:'d2', frequency: '587.330'},
-	{color:'gray',   title:'dis2', frequency: '622.254'},
-];
-
-var audioCtx = null;
-var volume = null;
-var oscillators = [];
-var oscillatorIds = 0;
-
-function playNote(dataset) {
-	if (null === audioCtx) {
-		return;
-	}
-
-	var oscillator = audioCtx.createOscillator();
-	oscillator.type = 'triangle'; // square triangle
-	oscillator.frequency.value = dataset.frequency;
-	oscillator.connect(volume);
-	oscillator.start();
-
-	++oscillatorIds;
-	oscillators[oscillatorIds] = oscillator;
-	dataset.oscillator = oscillatorIds;
-}
-
-function stopNote(dataset) {
-	if (null === audioCtx) {
-		return;
-	}
-
-	var id = parseInt(dataset.oscillator, 10);
-
-	setTimeout(
-		function() {
-			oscillators[id].stop();
-			delete oscillators[id];
-	}, 100);
-}
+var guitarColor = ['gray','black','red','yellow','gray','gray','gray','gray','gray','gray','gray','gray','gray','gray','gray','gray'];
 
 function onButtonStart() {
-	if (null === audioCtx) {
-		audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-
-		volume = audioCtx.createGain();
-		volume.connect(audioCtx.destination);
-	}
-	volume.gain.value = 0.25;
+	sound.reset();
+	sound.setVolume(.25);
 }
 
 function onTuneEnter(e) {
-	playNote(e.target.dataset);
+	sound.playNote(e.target.dataset);
 }
 
 function onTuneLeave(e) {
-	stopNote(e.target.dataset);
+	sound.stopNote(e.target.dataset);
 }
 
 function addHeader() {
@@ -77,10 +19,11 @@ function addHeader() {
 	divHeader.classList.add('header');
 	divGame.appendChild(divHeader);
 
-	guitarNeck.forEach(tone => {
+	var index = 0;
+	sound.getGuitarNeck().forEach(tone => {
 		var divTone = document.createElement('div');
 		divTone.classList.add('tone');
-		divTone.classList.add(tone.color);
+		divTone.classList.add(guitarColor[index]);
 		divTone.dataset.frequency = tone.frequency;
 		divHeader.appendChild(divTone);
 
@@ -88,12 +31,17 @@ function addHeader() {
 
 		divTone.addEventListener('mouseenter', onTuneEnter);
 		divTone.addEventListener('mouseleave', onTuneLeave);
-	});
 
+		++index;
+	});
+}
+
+function addStartButton() {
 	var buttonStart = document.createElement('button');
 	buttonStart.classList.add('start');
 	buttonStart.innerHTML = 'Start';
 	divGame.appendChild(buttonStart);
+
 	buttonStart.addEventListener('click', onButtonStart);
 }
 
@@ -102,5 +50,6 @@ window.onload = function()
 	divGame = document.getElementById('game');
 
 	addHeader();
+	addStartButton();
 };
 
---
+++ assets/module.sound.js
@@ -0,0 +1,117 @@
+var sound = (function () {
+
+	var guitarNeck = [
+		// https://de.wikipedia.org/wiki/Frequenzen_der_gleichstufigen_Stimmung
+		{title:'C4',  frequency: '261.626'},
+//		{title:'C#4', frequency: '277.183'},
+		{title:'D4',  frequency: '293.665'},
+//		{title:'D#4', frequency: '311.127'},
+		{title:'E4',  frequency: '329.628'},
+		{title:'F4',  frequency: '349.228'},
+//		{title:'F#4', frequency: '369.994'},
+		{title:'G4',  frequency: '391.995'},
+//		{title:'G#4', frequency: '415.305'},
+		{title:'A4',  frequency: '440'},
+//		{title:'A#4', frequency: '466.164'},
+		{title:'B4',  frequency: '493.883'},
+		{title:'C5',  frequency: '523.251'},
+//		{title:'C#5', frequency: '554.365'},
+		{title:'D5',  frequency: '587.330'},
+		{title:'D#5', frequency: '622.254'},
+		{title:'E5',  frequency: '659.255'},
+		{title:'F5',  frequency: '698.456'},
+//		{title:'F#5', frequency: '739.989'},
+		{title:'G5',  frequency: '783.991'},
+//		{title:'G#5', frequency: '830.609'},
+		{title:'A5',  frequency: '880'},
+//		{title:'A#5', frequency: '932.328'},
+		{title:'B5',  frequency: '987.767'},
+		{title:'C6',  frequency: '1046.50'},
+	];
+
+	var score = [
+		['E5', 4],
+		['E5', 4],
+		['E5', 4],
+		['C5', 8],
+		[0, 16],
+		['G5', 16],
+		['E5', 4],
+		['C5', 8],
+		[0, 16],
+		['G5', 16],
+		['E5', 4],
+		[0, 4],
+		['B5', 4],
+		['B5', 4],
+		['B5', 4],
+		['C6', 8],
+		[0, 16],
+		['G5', 16],
+		['D#5', 4],
+		['C5', 8],
+		[0, 16],
+		['G5', 16],
+		['E5', 4]
+	];
+
+	var audioCtx = null;
+	var volume = null;
+	var oscillators = [];
+	var oscillatorIds = 0;
+
+	function funcGetGuitarNeck() {
+		return guitarNeck;
+	}
+
+	function funcPlayNote(dataset) {
+		if (null === audioCtx) {
+			return;
+		}
+
+		var oscillator = audioCtx.createOscillator();
+		oscillator.type = 'triangle'; // square triangle
+		oscillator.frequency.value = dataset.frequency;
+		oscillator.connect(volume);
+		oscillator.start();
+
+		++oscillatorIds;
+		oscillators[oscillatorIds] = oscillator;
+		dataset.oscillator = oscillatorIds;
+	}
+
+	function funcStopNote(dataset) {
+		if (null === audioCtx) {
+			return;
+		}
+
+		var id = parseInt(dataset.oscillator, 10);
+
+		setTimeout(
+			function() {
+				oscillators[id].stop();
+				delete oscillators[id];
+		}, 100);
+	}
+
+	function funcReset() {
+		if (null === audioCtx) {
+			audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+
+			volume = audioCtx.createGain();
+			volume.connect(audioCtx.destination);
+		}
+	}
+
+	function funcSetVolume(value) {
+		volume.gain.value = value;
+	}
+
+	return {
+		getGuitarNeck: funcGetGuitarNeck,
+		playNote: funcPlayNote,
+		reset: funcReset,
+		setVolume: funcSetVolume,
+		stopNote: funcStopNote,
+	};
+}());
